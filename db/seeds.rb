ca210cb10a12911cf8933de56e483298d88b1a52
hanocha <hanocha.peter@gmail.com>
2018-11-02 01:08:45 +0900

    create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+switches = {
+  alps_blue: 'ALPS Blue',
+  alps_hulk: 'ALPS Hulk',
+  alps_white: 'ALPS White',
+  cherry_silent_red: 'Cherry Silent Red',
+  kailh_box_pink: 'Kailh Box Pink',
+  kailh_box_royal: 'Kailh Box Royal',
+  kailh_box_violet: 'Kailh Box Violet',
+  kailh_box_white: 'Kailh Box White',
+  kailh_pink: 'Kailh Pink',
+}
+cdn_url = "http://cdn.hanolab.net"
+
+swithces.each do |s|
+  Switch.create(
+    name: s,
+    image_url: "#{Settings.api_url}/#{s.key}.png",
+    sound_url: "#{Settings.api_url}/#{s.key}.mp3",
+  )
+end
